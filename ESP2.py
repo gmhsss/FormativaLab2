35be1b73c830113e74cf09bc1db0eab39f370d8e
Anthony Guimaraes <thonysutil@gmail.com>
2024-08-26 10:41:09 -0300

    atualzacao

--- ESP2.py
+++ ESP2.py
@@ -19,70 +19,75 @@ https://wokwi.com/arduino/projects/322577683855704658
 
 import network
 import time
+import _thread
 from machine import Pin
 from umqtt.simple import MQTTClient
 
 # MQTT Server Parameters
-MQTT_CLIENT_ID = "icaro"
+MQTT_CLIENT_ID = "esp2"
 MQTT_BROKER    = "broker.mqttdashboard.com"
 MQTT_USER      = ""
 MQTT_PASSWORD  = ""
 MQTT_TOPIC1    = "PontoH"
 MQTT_TOPIC2    = "PontoBe"
 
-
+# Configurações de pinos
 led = Pin(18, Pin.OUT)
+buzzer = Pin(5, Pin.OUT)
+slide_switch = Pin(15, Pin.IN, Pin.PULL_UP)  # Slide switch para desativar os alarmes
+
+hum = False
+alarme_ativo = False
 
 # Função de callback para recebimento de mensagens MQTT
 def callback(topic, msg):
-    #print("Mensagem recebida: ", msg.decode())
-    global status
-    if((str(msg.decode()) == "HAlta")):
+    global hum
+    if str(msg.decode()) == "HAlta":
         hum = True
-    elif(str((msg.decode()) == "HNormal")):
+    elif str(msg.decode()) == "HNormal":
         hum = False
 
+# Função para controle do buzzer e LED com threads
+def controle_alertas():
+    global hum, alarme_ativo
+    while True:
+        if hum and not slide_switch.value():  # Slide switch desligado (valor 0)
+            alarme_ativo = True
+            led.on()
+            buzzer.on()
+            time.sleep(1)
+            led.off()
+            buzzer.off()
+            time.sleep(1)
+        else:
+            led.off()
+            buzzer.off()
+            alarme_ativo = False
+        time.sleep(0.1)
 
+# Função principal
+def main():
+    print("Connecting to WiFi", end="")
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('PontoPe', 'milelinda')
+    while not sta_if.isconnected():
+        print(".", end="")
+        time.sleep(0.1)
+    print(" Connected!")
 
-print("Connecting to WiFi", end="")
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('PontoPe', 'milelinda')
-while not sta_if.isconnected():
-  print(".", end="")
-  time.sleep(0.1)
-print(" Connected!")
+    print("Connecting to MQTT server... ", end="")
+    client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
+    client.set_callback(callback)
+    client.connect()
+    client.subscribe(MQTT_TOPIC1)
+    print("Connected!")
 
+    # Inicia thread para controle de alertas
+    _thread.start_new_thread(controle_alertas, ())
 
-print("Connecting to MQTT server... ", end="")
-client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
-#Lembrem-se sempre é necessário ter um callback para receber informações.
-client.set_callback(callback)
-client.connect()
-client.subscribe(MQTT_TOPIC1)
-print("Connected!")
-
-teste = False
-
-
-status = 0
-while True:
-    client.check_msg()
-    time.sleep(1)
-    if hum and not teste:
-        
-        led.on()
+    while True:
+        client.check_msg()
         time.sleep(1)
-        led.off()
-        time.sleep(1)
 
-
-
-
-
-
-
-
-
-
-
+main()
